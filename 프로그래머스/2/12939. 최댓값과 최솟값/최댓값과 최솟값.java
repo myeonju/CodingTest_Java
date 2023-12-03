4daf2cf420672747a9aa82a48f37d0e7fdfa8b3c
myeonju <143158027+myeonju@users.noreply.github.com>
2023-12-03 23:43:13 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.55 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String s) {
+       
+        String[] numbers = s.split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(String number:numbers){ 
+            int num=Integer.parseInt(number);
+            min=Math.min(min,num);
+            max=Math.max(max, num);
+        }
+        
+        return min + " " + max;
+    }
+}
