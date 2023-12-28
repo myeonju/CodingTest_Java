1b1232c358f865bd364cc33ae078caf617307098
myeonju <143158027+myeonju@users.noreply.github.com>
2023-12-28 22:35:22 +0900

    [Bronze V] Title: 문자와 문자열, Time: 216 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        String S = scanner.next();
+        int i = scanner.nextInt();
+
+       System.out.println(S.charAt(i-1));
+    }
+}
