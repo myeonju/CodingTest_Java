eff65a97e7ba5659b12d52d55bfb0e90b32b972d
myeonju <143158027+myeonju@users.noreply.github.com>
2024-01-17 23:52:12 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 9.78 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,23 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder left = new StringBuilder();
-        int[] cnt = new int[food.length]; 
-        for(int i=1; i<food.length; i++){
-            if(food[i]%2==1){
-                food[i]--;
-            }
-            cnt[i] = food[i]/2;
+        
+        StringBuilder builder = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++) {
+            builder.append(Integer.toString(i).repeat(food[i] / 2));
         }
         
-        for(int i=1; i<cnt.length; i++){
-            for(int j=0; j<cnt[i]; j++){
-                left.append(String.valueOf(i)); 
-            }
-        }
-        
-        String right = new StringBuilder(left).reverse().toString();
-        
-        return left + "0" + right;
-       
+        return builder.toString() + "0" + builder.reverse().toString();
     }
+
 }
