195354c4a4df8946e6f0ae6352a46b2c7ee51142
myeonju <143158027+myeonju@users.noreply.github.com>
2024-01-31 00:09:24 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2392 ms, Memory: 186044 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Coordinates {
+    int x;
+    int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        List<Coordinates> coordinates = new ArrayList<>();
+
+        for(int i=0; i<N; i++){
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            coordinates.add(new Coordinates(x, y));
+        }
+
+        Collections.sort(coordinates, new Comparator<Coordinates>() {
+            @Override
+            public int compare(Coordinates c1, Coordinates c2) {
+                if(c1.x == c2.x){
+                    return c1.y - c2.y;
+                }
+                return c1.x - c2.x;
+            }
+        });
+
+        for(Coordinates coordinate: coordinates){
+            System.out.println(coordinate.x + " " + coordinate.y);
+        }
+    }
+}
