fa3ecb373e487fe3479ed78efbeff093dd89cc50
myeonju <143158027+myeonju@users.noreply.github.com>
2024-01-31 13:52:46 +0900

    [Silver II] Title: 좌표 압축, Time: 1784 ms, Memory: 237880 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] coordinates = new int[N];
+        int[] sortedCoordinates = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            sortedCoordinates[i] = coordinates[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(sortedCoordinates);
+
+        Map<Integer, Integer> compressedCoordinates = new HashMap<>();
+        int compressed = 0;
+        for(int s:sortedCoordinates){
+            if(!compressedCoordinates.containsKey(s)){
+                compressedCoordinates.put(s,compressed);
+                compressed++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int key:coordinates){
+          int result = compressedCoordinates.get(key);
+            sb.append(result).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
