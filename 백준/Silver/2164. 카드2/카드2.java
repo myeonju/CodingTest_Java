bbabd76abc8a30823f3591ef6ff1a3ec711e054f
myeonju <143158027+myeonju@users.noreply.github.com>
2024-02-01 16:13:13 +0900

    [Silver IV] Title: 카드2, Time: 276 ms, Memory: 56844 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=N; i++){
+            queue.add(i);
+        }
+
+        while (queue.size()> 1){
+            queue.poll();
+            int card = queue.poll();
+            queue.add(card);
+        }
+        int lastCard = queue.poll();
+        System.out.println(lastCard);
+    }
+}
