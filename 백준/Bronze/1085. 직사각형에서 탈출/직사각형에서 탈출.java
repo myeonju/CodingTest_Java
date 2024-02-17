54a974b80c614510e4b33d4cc020cae6ab2df766
myeonju <143158027+myeonju@users.noreply.github.com>
2024-02-18 00:29:22 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int w = scanner.nextInt();
+        int h = scanner.nextInt();
+
+        int minX = Math.min(x,w-x);
+        int minY = Math.min(y,h-y);
+
+        System.out.println(Math.min(minX,minY));
+    }
+}
