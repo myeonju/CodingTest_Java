abd967f9f457bd99837ca77875bec3bc149c1ff3
myeonju <143158027+myeonju@users.noreply.github.com>
2024-03-08 22:44:56 +0900

    [level 2] Title: 가장 큰 수, Time: 1.47 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,30 +2,30 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-      
-        String[] numberStr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            numberStr[i] = String.valueOf(numbers[i]);
+    
+        String[] strNumbers = new String[numbers.length];
+        
+        for(int i=0; i<numbers.length; i++){
+            strNumbers[i] = Integer.toString(numbers[i]);
         }
-
-        // 문자열을 조합하여 가장 큰 수를 만들 수 있도록 정렬
-        Arrays.sort(numberStr, new Comparator<String>() {
+        
+        Arrays.sort(strNumbers, new Comparator<String>(){
             @Override
             public int compare(String s1, String s2) {
                 return (s2 + s1).compareTo(s1 + s2);
             }
         });
-
-        // 배열을 문자열로 합치기
-        StringBuilder answerBuilder = new StringBuilder();
-        for (String str : numberStr) {
-            answerBuilder.append(str);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(String str: strNumbers){
+            sb.append(str);
         }
-
-        if (answerBuilder.charAt(0) == '0') {
+        
+        if(sb.charAt(0) == '0'){
             return "0";
         }
-
-        return answerBuilder.toString();
+        
+        return sb.toString();
     }
 }
