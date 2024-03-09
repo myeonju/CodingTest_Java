c2dddc581216b3ddc26f9d85f7620e009bb39df2
myeonju <143158027+myeonju@users.noreply.github.com>
2024-03-09 16:02:47 +0900

    [level 2] Title: H-Index, Time: 0.61 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,19 +2,25 @@ import java.util.*;
 
 class Solution {
     private boolean isValid(int[] citations, int h){
-        int count = 0;
+        int count=0;
+        
         for(int citation: citations){
-            if(citation>=h) count++;
+            if(citation >= h){
+                count++;
+            }
         }
-        return count>=h;
+        return count >= h;
     }
+    
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        int length = citations.length;
-        for(int h = length; h>=0; h--){
-            if(isValid(citations,h)) return h;
+        
+        for(int h = citations.length; h > 0; h--){
+            if(isValid(citations, h)){
+                return h;
+            }
         }
-
+        
         return 0;
     }
 }
