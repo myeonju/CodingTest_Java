aa5883dd9dc47243a38b252fe1a8dfd6335ac218
myeonju <143158027+myeonju@users.noreply.github.com>
2024-03-16 20:14:44 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    
+    private int maxDungeonsExplored=0;
+    
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        exploreDungeons(k, dungeons, visited, 0);
+        return maxDungeonsExplored;
+    }
+    
+    private void exploreDungeons(int k, int[][] dungeons, boolean[] visited, int count){
+     
+        for(int i=0; i<dungeons.length; i++){
+            if(!visited[i] && k>=dungeons[i][0]){
+                visited[i] = true;
+                exploreDungeons(k-dungeons[i][1], dungeons, visited, count+1);
+                visited[i] = false;
+            }
+        }
+        
+        maxDungeonsExplored = Math.max(maxDungeonsExplored, count);
+    }
+}
