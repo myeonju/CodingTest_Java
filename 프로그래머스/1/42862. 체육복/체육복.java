c29f122e48947723b7ce34adfa2b25858e3b4c0e
myeonju <143158027+myeonju@users.noreply.github.com>
2024-03-18 20:49:11 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] clothes = new int[n];
-        Arrays.fill(clothes,1);
+        int[] student = new int[n];
+        Arrays.fill(student,1);
         
         for(int l:lost){
-            clothes[l-1]--;
+            student[l-1]--;
         }
         
         for(int r:reserve){
-            clothes[r-1]++;
+            student[r-1]++;
         }
         
-        for (int i = 0; i < n; i++) {
-            if (clothes[i] == 0) {
-                if (i - 1 >= 0 && clothes[i - 1] == 2) {
-                    clothes[i - 1]--;
-                    clothes[i]++;
-                } else if (i + 1 < n && clothes[i + 1] == 2) {
-                    clothes[i + 1]--;
-                    clothes[i]++;
+        for(int i=0; i<n; i++){
+            if(student[i]==0){
+                if(i-1>=0 && student[i-1]==2){
+                    student[i-1]--;
+                    student[i]++;
+                } else if(i+1<n &&student[i+1]==2){
+                    student[i+1]--;
+                    student[i]++;
                 }
             }
         }
         
         int cnt=0;
-        for(int c: clothes){
-            if(c>0){
+        for(int s:student){
+            if(s>0){
                 cnt++;
             }
         }
