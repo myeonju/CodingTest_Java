7c48961c7141c57fbf75d0886e6410e5388bcae4
myeonju <143158027+myeonju@users.noreply.github.com>
2024-06-12 00:39:01 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
         
-        HashMap<String,Integer> map = new HashMap<>();
-        
-        for(int i=0; i<clothes.length; i++){
-            String category = clothes[i][1];
-            map.put(category, map.getOrDefault(category, 0)+1);
+        for (String[] c : clothes) {
+            String category = c[1];
+            map.put(category, map.getOrDefault(category, 0) + 1);
         }
         
         int answer = 1;
         
-        for(int val : map.values()){
-            answer*= (val+1);
+        for (int val : map.values()) {
+            answer *= (val + 1);    
         }
         
-        return answer-1;
+        return answer - 1;
     }
 }
